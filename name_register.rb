ef142fd9ae73c658840ab119451ce9329c846974
HIJIRI-MBP <zigzagoon17.5@gmail.com>
2020-03-13 10:58:31 +0900

    first commit

---
+++ name_register.rb
@@ -0,0 +1,66 @@
+# 名前の入力フォーム
+def input_name
+  p '名前か会社名を入力してください'
+  @name =gets.chomp
+  check_input_name(@name)
+end
+
+# 名前が空欄ではないかチェック
+def check_input_name(name)
+  if name.empty?
+    p '空欄では登録できません'
+    input_name
+  end
+end
+
+# 登録前の確認
+def confirmation(msg1,msg2)
+  p '以下の内容で登録します（１：はい ２：いいえ）'
+  p @name
+  select = gets.to_i
+  case select
+  when 1
+    p msg1
+  when 2
+    p msg2
+    input_name
+  else
+      p '１か２を選択してください'
+      confirmation(msg1,msg2)
+  end
+end
+
+# ファイルへ書き込む
+def file_write
+  open("namelist.txt", mode = "a") do |f|
+    f.puts @name
+    f.puts("--------------------------------------------")
+  end
+end
+
+# 登録を続けるかの確認
+def exit_confirmation(msg1,msg2)
+  p '登録を終了しますか？１：はい ２：いいえ'
+  select = gets.to_i
+  case select
+  when 1
+    p msg1
+    name_register
+  when 2
+    p msg2
+  else
+      p '１か２を選択してください'
+      exit_confirmation(msg1,msg2)
+  end
+end
+
+
+# 動作用
+def name_register
+  input_name
+  confirmation('登録完了','最初から入力し直してください')
+  file_write
+  exit_confirmation('登録へ戻ります','お疲れさまでした')
+end
+
+name_register
